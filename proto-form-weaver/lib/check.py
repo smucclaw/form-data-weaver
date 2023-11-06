eee92bf736faf248327ed27b788e8ec5995c251e
ymh <hanyongming@gmail.com>
2023-11-06 19:30:53 +0800

    scaffold python app

---
+++ proto-form-weaver/lib/check.py
@@ -0,0 +1,18 @@
+import typer
+from typing import Optional
+from pathlib import Path
+from dataclasses import dataclass
+import yaml
+
+
+def check_encoding_with_schema(config):
+    typer.echo("Checking .le encoding against JSON schema...")
+
+
+
+def check_encoding_typos(config): 
+    typer.echo("Checking .le encoding for typos...")
+
+
+def do_all_checks(config): 
+    pass
